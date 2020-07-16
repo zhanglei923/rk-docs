296282aea43dee1c8453508f1e012b8381d1a539
zhanglei <zhanglei@xiaoshouyi.com>
2020-07-16 19:35:35 +0800

    up

--- cmdParsers/watchdog/watch.sh
+++ cmdParsers/watchdog/watch.sh
@@ -1,3 +1,4 @@
 # sh /home/ingage/autopack/docs-ingage-frontendedge/automatic/autopack/watchdog/watch.sh
-cd /home/ingage/applications/LOCAL_WEB_CDN_MAINTAINER/master_projects/docs-ingage-frontendedge/automatic/autopack-local_cdn/watchdog
-node watch_minutes
+cd /home/ingage/applications/LOCAL_WEB_CDN_MAINTAINER/master_projects/rk-docs/cmdParsers/watchdog
+git pull
+node watch
